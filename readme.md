5bfdc72817aab6b6ace6758631d0f2704dd3bf93
mrvanagas <lukonelis1@gmail.com>
2021-12-05 20:46:58 +0200

    feat: scraper now exports scraped data into CSV

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 This is a scraper built with axios and cheerio and Javascript. 
+To run the app, pull the code from the repository, install dependancies with "npm install" in the terminal and run the "node scrape" command in the terminal. In its current configuration, there is only 1 page load/request per app run.
 
-In its current configuration, it is meant to scrape fly540.com airlines for departing airports, flight dates, flight times and flight prices. Currently, it outputs data in a JSON format, until I figure out on how to export them into CSV format.
+In order for the scraper to work, a link, with relevant search settings, has to be placed in the URL identifier.
+
+In its current configuration, it is meant to scrape fly540.com airlines for departing airports, flight dates, flight times and flight prices. When the data is first scraped, it is placed in an object array, then it is converted to a string and exported into a CSV file.
--- scrape.js
+++ scrape.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require('fs')
+const writeStream = fs.createWriteStream('post.csv');
 
 
 // URL of the page we want to scrape
@@ -10,13 +11,13 @@ const url = "";
 async function scrapeData() {
   try {
     const { data } = await axios.get(url)
-
     const $ = cheerio.load(data)
-
     const flight_info = $('.fly5-result')
 
+    //Array where scraped data will be stored
     const htmlData = []
 
+    //Scraping info placed into relevant objects
     flight_info.each ((i, el) => {
         const flight_info = {airport_name: '', flight_dates:'', flight_time: '', price: ''}
         flight_info.airport_name = $(el)
@@ -35,16 +36,31 @@ async function scrapeData() {
         flight_info.price = $(el)
         .find('.flprice, .flcur')
         .text()
-
+        //Scraped info objects pushed into array
         htmlData.push(flight_info)
     })
     console.dir(htmlData)
-    fs.writeFile('flights.json', JSON.stringify(htmlData, null, 4), (err) => {
-      if (err) {
-        console.error(err)
-        return;
-      }
-    })
+
+    //Converts array objects to CSV string
+    const csvString = [
+      [
+      "airport name",
+      "flight_dates",
+      "flight_time",
+      "price"
+    ],
+    ...htmlData.map(flight_info => [
+      flight_info.airport_name,
+      flight_info.flight_dates,
+      flight_info.flight_time,
+      flight_info.price
+      ])
+    ]
+    .map(e => e.join(';'))
+    .join("\n")
+
+    //Export data into CSV file
+    writeStream.write(csvString)
     console.log("Finished scraping");
     
     
